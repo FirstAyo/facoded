b2e8f97a785ec9f5af26c00a4ea4714ecb58b53b
Festus <techarenaz01@gmail.com>
2024-10-05 00:23:24 -0700

    Added the search component to the homepage

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,13 @@
+
+function SearchInput() {
+    return (
+
+        <>
+            <div className="mt-20 w-[50%] mx-auto">
+                <input type="text" className="shadow-lg border-2 border-red-900 rounded-md py-1 px-5 w-[100%] h-12" id="intro-box"/>
+            </div>
+        </>
+    )
+}
+
+export default SearchInput
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import Hero from "../components/Hero"
+import SearchInput from "../components/SearchInput"
 
 function HomePage() {
     return (
         <>
             <section className="">
                 <Header />
+                <SearchInput />
                 <Hero />
             </section>
         </>
