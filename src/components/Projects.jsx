d860a89da8b2e87ae12084d014fb7159c77d0590
Festus <techarenaz01@gmail.com>
2024-10-05 21:21:06 -0700

    finsihed the css for the projects components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,26 @@ function Projects() {
     return (
         <>
 
-        
+                <article className="py-5 px-5 rounded-xl flex flex-wrap gap-10">
+
+                    {postSummaries.map((postSummary) => (
+                        <div key={postSummary.id} className='grow basis-[250px] flex-1 flex flex-col border-2 border-red-900 rounded-md px-5 py-5'>
+                            <img src={comfortImage} alt="" className="w-full h-[185px] flex-1" />
+                            <h2 className="font-bold my-3 flex-1">{postSummary.title}</h2>
+                            <p className="text-sm text-gray-300 flex-1">{postSummary.description}</p>
+                            <div className="mt-10 flex flex-row gap-3 w-[90%] mx-auto md:w-full flex-1">
+                                <button className="bg-red-900 rounded-full px-4 py-2 text-xs flex-1 md:px-1 md:py-2 text-white">Live
+                                    Preview</button>
+                                <button
+                                    className="bg-transparent border-2 rounded-full px-4 py-2 text-xs flex-1 md:px-1 md:py-2">Check
+                                    on
+                                    GitHub</button>
+                            </div>
+
+                        </div>
+                    ))}
+
+                </article>
 
         </>
     )
