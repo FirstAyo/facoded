38d6e39a986e5052c7c696f4783871024189be34
Festus <techarenaz01@gmail.com>
2024-10-07 19:41:55 -0700

    added a new project menu to the My Projects Page

---
+++ src/components/myprojects/ProjectHeader.jsx
@@ -0,0 +1,35 @@
+import projectMenuLinks from '../../projectMenu.json';
+import logo from '../../../public/main-logo.svg';
+import { Link } from 'react-router-dom';
+
+function ProjectHeader() {
+  return (
+    <>
+    <section className="shadow-md w-[100%]">
+        <header
+            className="flex justify-between items-center w-[90%] mx-auto py-5 text-black lg:sticky lg:top-3">
+                <img src= { logo } alt="" className='w-24 h-12' />
+
+            {/* <h1 className="text-red-900 font-bold text-2xl">FaCoded</h1> */}
+
+            <nav
+                className="border-[#FFDBD9] border-2 lg:border-none nav-links duration-500 absolute top-[-100%] w-[80.1%] lg:w-auto min-h-[35vh] mx-auto flex items-center px-5 lg:min-h-fit lg:static">
+                <ul className="flex flex-col gap-6 lg:flex-row justify-between lg:gap-10 my-5 lg:my-0 text-lg font-semibold">
+                    { projectMenuLinks.map ((projectMenuLink => (
+                        <Link to={ projectMenuLink.path } key={ projectMenuLink.id }> { projectMenuLink.menu }</Link>
+                    )))}
+                    
+                    <button className="bg-red-950 rounded-full px-5 py-2 lg:hidden" id="resume-btn">My Resume</button>
+                </ul>
+            </nav>
+
+            <div className="flex items-center gap-6">
+                <button className="bg-red-900 rounded-md px-10 py-3 text-white lg:block" id="resume-btn">My Resume</button>
+            </div>
+        </header>
+    </section>
+</>
+  )
+}
+
+export default ProjectHeader
--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,19 +1,20 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import ProjectHeader from "../components/myprojects/ProjectHeader"
 import Projects from "../components/Projects"
 
 function MyProjects() {
-  return (
-    <>
+    return (
+        <>
 
-    <section>
-        <Header />
-        <Projects />
-        <Footer />
-    </section>
-    
-    </>
-  )
+            <section>
+                <ProjectHeader />
+                <Projects />
+                <Footer />
+            </section>
+
+        </>
+    )
 }
 
 export default MyProjects
