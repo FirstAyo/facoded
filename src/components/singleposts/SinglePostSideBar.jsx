0e0462b31d69c416d8d31faf85659d982cd90590
Festus <techarenaz01@gmail.com>
2024-10-12 10:26:42 -0700

    limited the number posts in the sidebar to 5 random posts

--- src/components/singleposts/SinglePostSideBar.jsx
+++ src/components/singleposts/SinglePostSideBar.jsx
@@ -2,18 +2,24 @@ import { Link } from 'react-router-dom';
 import postContents from '../../posts.json';
 
 function SinglePostSideBar() {
-  return (
-    <>
-    <section>
-        {postContents.map ((postContent) => (
-            <div key={postContent.id}>
-                <img src= {postContent.image} alt="" />
-                <Link><h2>{postContent.title}</h2></Link>
-            </div>
-        ))}
-    </section>
-    </>
-  )
+
+    const randomPosts = [];
+    for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * postContents.length);
+        randomPosts.push(postContents[randomIndex]);
+    }
+    return (
+        <>
+            <section>
+                {randomPosts.map((postContent) => (
+                    <div key={postContent.id}>
+                        <img src={postContent.image} alt="" className='w-[100%]' />
+                        <Link><h2>{postContent.title}</h2></Link>
+                    </div>
+                ))}
+            </section>
+        </>
+    )
 }
 
 export default SinglePostSideBar
