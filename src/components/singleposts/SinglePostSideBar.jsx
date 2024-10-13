b1bdddd1413a00e3985daaee4043fbc7816f8fcb
Festus <techarenaz01@gmail.com>
2024-10-12 21:24:16 -0700

    completed the side bar styling and design

--- src/components/singleposts/SinglePostSideBar.jsx
+++ src/components/singleposts/SinglePostSideBar.jsx
@@ -10,11 +10,13 @@ function SinglePostSideBar() {
     }
     return (
         <>
-            <section>
+            <section className='font-mono flex flex-col gap-5'>
                 {randomPosts.map((postContent) => (
-                    <div key={postContent.id}>
+                    <div key={postContent.id} className='flex flex-col border border-red-800 px-4 py-3'>
                         <img src={postContent.image} alt="" className='w-[100%] h-52' />
-                        <Link><h2>{postContent.title}</h2></Link>
+                        <p className='text-red-800 font-semibold w-[40%] py-2'>{ postContent.category }</p>
+                        <h2 className='text-xl font-semibold py-1'>{postContent.title}</h2>
+                        <Link>Read More...</Link>
                     </div>
                 ))}
             </section>
