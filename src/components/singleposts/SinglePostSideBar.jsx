6f6699d4a806e4eb2c081ee27e149ab53588cf2f
Festus <techarenaz01@gmail.com>
2024-10-13 20:05:17 -0700

    solved the unique key error for the random posts in the sidebar component

--- src/components/singleposts/SinglePostSideBar.jsx
+++ src/components/singleposts/SinglePostSideBar.jsx
@@ -3,18 +3,22 @@ import postContents from '../../posts.json';
 
 function SinglePostSideBar() {
 
-    const randomPosts = [];
-    for (let i = 0; i < 5; i++) {
-        const randomIndex = Math.floor(Math.random() * postContents.length);
-        randomPosts.push(postContents[randomIndex]);
+    let uniqueRandomNumbers = new Set();
+
+    while (uniqueRandomNumbers.size < 5) {
+        let randomIndex = Math.floor(Math.random() * postContents.length);
+        uniqueRandomNumbers.add(randomIndex);
     }
+
+    const randomPosts = Array.from(uniqueRandomNumbers).map((index) => postContents[index]);
+
     return (
         <>
             <section className='font-mono flex flex-col md:grid md:grid-cols-2 lg:grid-cols-1 gap-8'>
                 {randomPosts.map((postContent) => (
                     <div key={postContent.id} className='flex flex-col shadow-xl border-red-800 px-4 py-3'>
                         <img src={postContent.image} alt="" className='w-[100%] h-52' />
-                        <p className='text-red-800 font-semibold w-[40%] py-2'>{ postContent.category }</p>
+                        <p className='text-red-800 font-semibold w-[40%] py-2'>{postContent.category}</p>
                         <h2 className='text-xl font-semibold py-1'>{postContent.title}</h2>
                         <Link>Read More...</Link>
                     </div>
