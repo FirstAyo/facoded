30cac32262d8e2ee8fe9b2a6434d47756ee7b76e
Festus <techarenaz01@gmail.com>
2024-10-14 23:09:53 -0700

    added an alert function for the searchinput component

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,11 +1,16 @@
 
 function SearchInput() {
+
+    function searchButtonAlert () {
+        alert("I am working on making the search query functional");
+    }
+
     return (
 
         <>
             <div className="flex mt-20 mb-5 w-[90%] lg:w-[50%] mx-auto font-mono font-semibold">
                 <input type="text" className="shadow-lg border border-red-900 rounded-l py-1 px-5 w-[70%] h-12" id="intro-box" />
-                <button className="text-white bg-red-800 w-[30%] rounded-r shadow-lg">Search</button>
+                <button onClick={searchButtonAlert} className="text-white bg-red-800 w-[30%] rounded-r shadow-lg">Search</button>
             </div>
         </>
     )
