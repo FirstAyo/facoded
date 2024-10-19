3a3fe553eb23f53c61d97c2164f5c57681273211
Festus <techarenaz01@gmail.com>
2024-10-19 13:45:28 -0700

    edited the single post to add it as a component and pass props to displaty the post

--- src/components/singleposts/SinglePosts.jsx
+++ src/components/singleposts/SinglePosts.jsx
@@ -1,21 +1,22 @@
 // import featuredImg from '../../../public/fitnessPage.jpeg'
-import { useParams} from 'react-router-dom';
-import postData from '../../posts.json';
+// import { useParams} from 'react-router-dom';
+import post from '../../posts.json';
+import PropTypes from 'prop-types';
 
-function SinglePosts() {
+function SinglePosts({ post }) {
 
-    const { id } = useParams();
-    const postId = parseInt(id, 10);
+    // const { id } = useParams();
+    // const postId = parseInt(id, 10);
 
-    const posts = postData.find(function (post) {
-        return post.id === postId;
-    });
+    // const posts = postData.find(function (post) {
+    //     return post.id === postId;
+    // });
 
-    if (!posts) {
-        return <div>Post not found</div>;
-    }
+    // if (!posts) {
+    //     return <div>Post not found</div>;
+    // }
 
-    const paragraphs = posts.post;
+    const paragraphs = post.post;
 
     return (
         <>
@@ -23,12 +24,12 @@ function SinglePosts() {
             <section className='px-5 py-3 font-mono'>
 
                 <div className='w-[95%] mx-auto border-b border-red-800'>
-                    <p className='text-red-800 font-semibold py-1'> {posts.category} </p>
-                    <h2 className='text-xl tracking-wide font-semibold py-2 md:text-3xl'>{posts.title}</h2>
+                    <p className='text-red-800 font-semibold py-1'> {post.category} </p>
+                    <h2 className='text-xl tracking-wide font-semibold py-2 md:text-3xl'>{post.title}</h2>
                 </div>
 
                 <div className='w-[95%] mx-auto h-64 md:h-96'>
-                    <img src={posts.image} alt="" className='py-5 w-[100%] h-[100%]' />
+                    <img src={post.image} alt="" className='py-5 w-[100%] h-[100%]' />
                 </div>
 
                 <div className='w-[95%] mx-auto'>
@@ -40,6 +41,10 @@ function SinglePosts() {
             </section>
         </>
     )
+}
+
+SinglePosts.propTypes = {
+    post: PropTypes.object.isRequired,
 }
 
 export default SinglePosts
--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -6,21 +6,24 @@ import SinglePostSideBar from "../components/singleposts/SinglePostSideBar"
 import { useParams, Link } from 'react-router-dom';
 import postData from '../posts.json';
 import Header from "../components/Header";
+import SinglePosts from "../components/singleposts/SinglePosts";
 
 function SinglePost() {
 
     const { id } = useParams();
     const postId = parseInt(id, 10);
 
-    const posts = postData.find(function (post) {
-        return post.id === postId;
-    });
+    // const posts = postData.find(function (post) {
+    //     return post.id === postId;
+    // });
 
-    if (!posts) {
+    const post = postData.find(post => post.id === postId);
+
+    if (!post) {
         return <div>Post not found</div>;
     }
 
-    const paragraphs = posts.post;
+    // const paragraphs = posts.post;
 
     function viewLiveButton () {
         alert("This webpage is not hosted yet. I am working on hosting it soon. Please come back and click this button in a few days time. Thank you")
@@ -36,26 +39,11 @@ function SinglePost() {
 
             <div className="w-[95%] mx-auto flex flex-col gap-5 lg:flex-row my-10 font-mono">
                 <div className="lg:w-[70%] shadow-2xl">
-                    <section className='px-5 py-3'>
-
-                        <div className='w-[95%] mx-auto border-b border-red-800'>
-                            <p className='text-red-800 font-semibold py-1'> {posts.category} </p>
-                            <h2 className='text-xl tracking-wide font-semibold py-2 md:text-3xl'>{posts.title}</h2>
-                        </div>
-
-                        <div className='w-[95%] mx-auto h-64 md:h-96'>
-                            <img src={posts.image} alt="" className='py-5 w-[100%] h-[100%]' />
-                        </div>
-
-                        <div className='w-[95%] mx-auto'>
-                            {paragraphs.map(function (paragraph, index) {
-                                return <p key={index} className='py-2'>{paragraph}</p>;
-                            })}
-                        </div>
-                    </section>
+                    <SinglePosts post = {post} />
+                        
                     <div className="w-[90%] mx-auto my-10 flex gap-3 flex-col md:flex-row">
                         <Link onClick={viewLiveButton} className="flex-1 text-center py-5 rounded-sm bg-red-800 text-white text-xl font-semibold">Live View</Link>
-                        <Link to={posts.gitHubPath} className="flex-1 text-center py-5 rounded-sm bg-transparent border-2 border-red-800 text-red-800 text-xl font-semibold">View on GitHub</Link>
+                        <Link to={post.gitHubPath} className="flex-1 text-center py-5 rounded-sm bg-transparent border-2 border-red-800 text-red-800 text-xl font-semibold">View on GitHub</Link>
                     </div>
                 </div>
                 <div className="lg:w-[30%] shadow-2xl">
